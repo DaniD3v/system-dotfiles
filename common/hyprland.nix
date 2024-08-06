b727eaede23773f7cc62685228da519ed93fc131
DaniD3v <sch220233@spengergasse.at>
2024-08-06 19:45:34 +0200

    module hyprland: removed redundant xdg-desktop-portal (xdg-desktop-portal-hyprland is already added by the hyprland module)

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -1,8 +1,7 @@
 {pkgs, ...}: {
   programs.hyprland.enable = true;
 
-  xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-hyprland
+  xdg.portal.extraPortals = [
+    pkgs.xdg-desktop-portal-gtk
   ];
 }
