accafa5e6897da7371a1feb4cf61778f4102433e
DaniD3v <sch220233@spengergasse.at>
2025-03-12 00:48:52 +0100

    feat(server): forward ssh port and allow logging into user account

--- hosts/server.nix
+++ hosts/server.nix
@@ -2,18 +2,27 @@
   users.users.notyou = {
     isNormalUser = true;
     extraGroups = ["wheel"];
+
+    openssh.authorizedKeys.keyFiles = [../ssh_key.pub];
   };
 
+  # basic tools
   environment.systemPackages = with pkgs; [
     helix
     btop
     git
   ];
 
+  # static ip
   networking.interfaces."enp15s0".ipv4.addresses = [
     {
       address = "192.168.1.2";
       prefixLength = 24;
     }
   ];
+
+  # forward ssh
+  networking.firewall = {
+    allowedTCPPorts = [22];
+  };
 }
